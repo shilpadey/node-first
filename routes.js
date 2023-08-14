34425c3d0cffcce5345a537bba2cced27697b238
shilpadey <shilpadey735@gmail.com>
2023-08-14 10:40:13 +0530

    debug and autorestart

--- routes.js
+++ routes.js
@@ -16,9 +16,8 @@ const requestHandler = (req,res) => {
       res.write(`<body>${data}</body>`)
       res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
       res.write('</html>');
-      return res.end();
+      res.end();
     });
-    
     
   }else if (url === '/message' && method === 'POST') {
     req.on('data', (chunk) => {
@@ -34,15 +33,15 @@ const requestHandler = (req,res) => {
         return res.end();
       });
     });
-    
-  }else {
-    //res.setHeader('Content-Type', 'text/html');
+  }else{
+    res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title> my first web page</title></head>')
     res.write('<body><h1>Welcome to my Node Js project</h1></body>');
     res.write('</html>')
     res.end();
   };
+  
 }
 
 //module.exports = requestHandler;
