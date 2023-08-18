75bce2b368bd50207b5523e4f38f68fb9cad765f
shilpadey <shilpadey735@gmail.com>
2023-08-18 12:59:28 +0530

    middleware, bodyparser, request

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const http = require('http');
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
     console.log('In the middleware');
-    next();
+    res.send('<form action="/product" method="POST"><input type="text" name="title"/><input type="number" name="size"/><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In the another middleware');
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello to Node.js</h1>');
 });
 
