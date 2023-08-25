366670a096c5027139eaebf62f9e9a063ac00040
shilpadey <shilpadey735@gmail.com>
2023-08-25 12:51:45 +0530

    models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProducts = (req, res, next) => {
     res.render('add-product', {
@@ -11,19 +11,23 @@ exports.getAddProducts = (req, res, next) => {
 };
 
 exports.postAddProducts = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/shop/products');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/shop/products',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/shop/products',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
+    
 };
 
 exports.getContacts = (req, res, next) => {
